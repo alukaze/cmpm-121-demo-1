f6e32746ddc69efb5910f391c7a3f02bbced89af
Alan Lu <alu36@ucsc.edu>
2024-10-15 23:24:07 -0700

    Step 5 Done

--- src/main.ts
+++ src/main.ts
@@ -16,39 +16,64 @@ document.body.appendChild(button);
 
 // Counter
 let beers = 0;
+let oneClick = 1;
 
 const counter = document.createElement("div");
 counter.textContent = "Beers: 0";
 app.append(counter);
 
 button.addEventListener("click", () => {
-  beers++;
+  beers += oneClick;
   counter.textContent = "Beers: " + beers;
+  unlockUpgrade();
 });
 
-// Auto Counter
-function autoCount() {
-  beers++;
-  counter.textContent = "Beers: " + beers;
+// Upgrade Button
+const upgradeButton = document.createElement("button");
+upgradeButton.textContent = "Upgrade #1";
+upgradeButton.disabled = true; 
+document.body.appendChild(upgradeButton);
+
+const unlockUpgrade = () => {
+    if (beers >= 10) {
+        upgradeButton.disabled = false;
+    } else {
+        upgradeButton.disabled = true;
+    }
 }
-const autoInterval = 5000;
-setInterval(autoCount, autoInterval);
+
+upgradeButton.addEventListener("click", () => {
+    beers -= 10;
+    counter.textContent = "Beers: " + beers;
+    oneClick ++;
+})
+
+// // Auto Counter
+// function autoCount() {
+//   beers ++;
+//   counter.textContent = "Beers: " + beers;
+//   unlockUpgrade();
+// }
+// const autoInterval = 5000;
+// setInterval(autoCount, autoInterval);
 
 // Continuous Growth
 // Check time
 let frameTime = performance.now();
 const updateCounter = () => {
-    // Check time again
-    const frame = performance.now();
-    // Time elapased between checks
-    const deltaTime = (frame - frameTime) / 1000;
-    frameTime = frame;
+  // Check time again
+  const frame = performance.now();
+  // Time elapased between checks
+  const deltaTime = (frame - frameTime) / 1000;
+  frameTime = frame;
 
-    beers += deltaTime;
-    counter.textContent = "Beers: " + Math.floor(beers);
+  beers += deltaTime;
+  counter.textContent = "Beers: " + Math.floor(beers);
+  unlockUpgrade();
 
-    // Run every frame
-    requestAnimationFrame(updateCounter);
-}
-// Start loop 
+  // Run every frame
+  requestAnimationFrame(updateCounter);
+};
+// Start loop
 requestAnimationFrame(updateCounter);
+
