dd7a5f33ab02af2a132ba8565a286683906dcc9e
Alan Lu <alu36@ucsc.edu>
2024-10-15 22:50:17 -0700

    Step 4 Done

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ document.body.appendChild(button);
 // Counter
 let beers = 0;
 
-const counter = document.createElement("counting");
+const counter = document.createElement("div");
 counter.textContent = "Beers: 0";
 app.append(counter);
 
@@ -28,8 +28,27 @@ button.addEventListener("click", () => {
 
 // Auto Counter
 function autoCount() {
-    beers++;
-    counter.textContent = "Beers: " + beers;
+  beers++;
+  counter.textContent = "Beers: " + beers;
 }
 const autoInterval = 5000;
-setInterval (autoCount, autoInterval)
+setInterval(autoCount, autoInterval);
+
+// Continuous Growth
+// Check time
+let frameTime = performance.now();
+const updateCounter = () => {
+    // Check time again
+    const frame = performance.now();
+    // Time elapased between checks
+    const deltaTime = (frame - frameTime) / 1000;
+    frameTime = frame;
+
+    beers += deltaTime;
+    counter.textContent = "Beers: " + Math.floor(beers);
+
+    // Run every frame
+    requestAnimationFrame(updateCounter);
+}
+// Start loop 
+requestAnimationFrame(updateCounter);
