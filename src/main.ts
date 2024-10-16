93b4f28b452b9dd42e883630508e017e5735f64e
Alan Lu <alu36@ucsc.edu>
2024-10-15 21:17:22 -0700

    Step 2 Done

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,21 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-const button = document.createElement('button');
-button.textContent = '🍺';
+// Button
+const button = document.createElement("button");
+button.textContent = "🍺";
 document.body.appendChild(button);
+
+// Counter
+let beers = 0;
+
+const counter = document.createElement('counting');
+counter.textContent = "Beers: 0";
+app.append(counter);
+
+button.addEventListener("click", () => {
+    beers ++;
+    counter.textContent = 'Beers: ' + beers;
+})
+
+// Auto Counter
