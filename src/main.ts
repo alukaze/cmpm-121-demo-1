c3a9302efdfb07f839eec6ef786ab84891fe6665
Alan Lu <alu36@ucsc.edu>
2024-10-15 21:02:44 -0700

    step 1 done

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,13 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "Gotta Click Em All";
+const gameName = "Beertiful Clicking";
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const button = document.createElement('button');
+button.textContent = '🍺';
+document.body.appendChild(button);
